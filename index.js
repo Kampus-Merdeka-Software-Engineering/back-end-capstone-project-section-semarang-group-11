e715915222347762d75358d9c9b1c83e03f72bdf
Hana Farahdiana <72843256+hanafarahd@users.noreply.github.com>
2023-10-10 10:18:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import loginRoutes from "./routes/login.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
